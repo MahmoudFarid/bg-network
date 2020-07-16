23e9185eba770277dec33f18b1696c90aadfbe02
halafarid <hala.farid179@gmail.com>
2020-07-17 00:15:23 +0200

    Initialize Project

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "../public/styles/tailwind.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <h1>Hello BG-Network</h1>
+    </div>
+  );
+}
